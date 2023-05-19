3cb2052b7a65034a832a2ad0f945fe8ced2511b1
nagshkere00 <134064513+nagshkere00@users.noreply.github.com>
2023-05-19 15:17:46 +0200

    Create app.py

---
+++ webserver 1 /app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os, psutil
+import socket
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname} <h3> This is from VM7 </h3> <br/>"
+    return html.format(name=os.getenv("NAME", "Kurt"), hostname=socket.gethostname())
+
+@app.route("/load")
+def load():
+    load=psutil.cpu_percent()
+#    html = "<h4>Hello....check percentage ....{See}"
+    return str(load) #html.format(See=load)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4000)
